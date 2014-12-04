b246631640c1de5c8c3ec1a8f40a5004b5ee705c
izurururu911 <izuru.okumura@gmail.com>
2014-12-04 20:02:28 +0900

    Initial Commit

---
+++ aaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaa
+//
+//  Created by 奥村 維敦 on 2014/12/04.
+//  Copyright (c) 2014年 奥村 維敦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaa
+//
+//  Created by 奥村 維敦 on 2014/12/04.
+//  Copyright (c) 2014年 奥村 維敦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
