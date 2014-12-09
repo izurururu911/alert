5ad8d543099b27f95bb24721f1b7e2685a74f5b0
izurururu911 <izuru.okumura@gmail.com>
2014-12-09 15:38:08 +0900

    first commit

--- aaa/ViewController.h
+++ aaa/ViewController.h
@@ -11,5 +11,12 @@
 @interface ViewController : UIViewController
 
 
+{
+}
+-(IBAction)push1;
+-(IBAction)push2;
+-(IBAction)push3;
+
+
 @end
 
--- aaa/ViewController.m
+++ aaa/ViewController.m
@@ -8,20 +8,68 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIAlertViewDelegate>
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIAlertView *alert1 =[[UIAlertView alloc] initWithTitle:@"いづちん"
+                                                   message:@"こんにちは"
+                                                  delegate:self
+                                         cancelButtonTitle:@"キャンセル"
+                                         otherButtonTitles:@"OK", nil];
+    [alert1 show];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(IBAction)push1{
+    
+    UIAlertView *alert1 =[[UIAlertView alloc] initWithTitle:@"いづちん"
+                                                    message:@"こんにちは"
+                                                   delegate:self
+                                          cancelButtonTitle:@"キャンセル"
+                                          otherButtonTitles:nil];
+    [alert1 show];
+
+}
+
+-(IBAction)push2{
+    
+    UIAlertView *alert2 =[[UIAlertView alloc] initWithTitle:@"いづちん"
+                                                    message:@"こんにちは"
+                                                   delegate:self
+                                          cancelButtonTitle:@"キャンセル"
+                                          otherButtonTitles:@"いづる",@"おくむら",nil];
+    [alert2 show];
+}
+
+-(IBAction)push3{
+    
+    UIAlertView *alert3 =[[UIAlertView alloc] initWithTitle:@"いづちん"
+                                                    message:@"こんにちは"
+                                                   delegate:self
+                                          cancelButtonTitle:@"キャンセル"
+                                          otherButtonTitles:@"OK",nil];
+    alert3.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
+    [alert3 show];
+    
+}
+
+
+
+
 
 @end
